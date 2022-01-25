ac99fc8d4279031a399f63b73404ae29f4d5ee73
eric2788 <tsukiko852@gmail.com>
2022-01-25 17:06:02 +0800

    add iso format and change timezone func

--- datetime/datetime.go
+++ datetime/datetime.go
@@ -1,19 +1,47 @@
 package datetime
 
 import (
+	"fmt"
 	"time"
 )
 
+const (
+	TimeFormat = "2006-01-02 15:04:05"
+	ISOFormat  = "2006-01-02T15:04:05Z07"
+)
+
+func ToTimeZone(iso string, location *time.Location) (time.Time, error) {
+	t, err := ParseISOStr(iso)
+	if err != nil {
+		return t, err
+	} else if location == nil {
+		return t, fmt.Errorf("location is null")
+	}
+	return t.In(location), nil
+}
+
+func FormatISO(time time.Time) string {
+	return time.Format(ISOFormat)
+}
+
+func FormatISOFromStr(timeStr string) (string, error) {
+	t, err := time.Parse(TimeFormat, timeStr)
+	if err != nil {
+		return "", err
+	}
+	return FormatISO(t), nil
+}
+
 func FormatSeconds(ts int64) string {
-	return FromSeconds(ts).Format("2006-01-02 15:04:05")
+	return FromSeconds(ts).Format(TimeFormat)
 }
 
 func ParseISOStr(iso string) (time.Time, error) {
-	return time.Parse("2006-01-02T15:04:05Z07", iso)
+	return time.Parse(ISOFormat, iso)
 }
 
 func FormatMillis(ts int64) string {
-	return time.UnixMilli(ts).Format("2006-01-02 15:04:05")
+	return time.UnixMilli(ts).Format(TimeFormat)
 }
 
 func Duration(before, after int64) time.Duration {
--- datetime/datetime_test.go
+++ datetime/datetime_test.go
@@ -19,6 +19,19 @@ func TestDuration(t *testing.T) {
 	assert.Equal(t, time.Duration(24), Duration(before, after)/time.Hour)
 }
 
+func TestChangeTimeZone(t *testing.T) {
+	oTime := "2022-01-24T20:44:51Z"
+	hk, err := time.LoadLocation("Asia/Hong_Kong")
+	if err != nil {
+		t.Fatal(err)
+	}
+	changed, err := ToTimeZone(oTime, hk)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "2022-01-25T04:44:51+08", FormatISO(changed))
+}
+
 func TestParseISO(t *testing.T) {
 	iso := "2021-09-01T13:24:29Z"
 	date, err := ParseISOStr(iso)
