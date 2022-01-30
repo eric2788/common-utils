298849df4cd96c2fa3670001d295d0687b55969a
eric2788 <tsukiko852@gmail.com>
2022-01-30 19:35:07 +0800

    test multiple timezone

--- datetime/datetime_test.go
+++ datetime/datetime_test.go
@@ -2,6 +2,7 @@ package datetime
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 	"time"
 )
@@ -45,12 +46,32 @@ func TestChangeTimeZone(t *testing.T) {
 
 }
 
-// only work when timezone is UTC+8
 func TestParseISO(t *testing.T) {
 	iso := "2021-09-01T13:24:29Z"
 	date, err := ParseISOStr(iso)
 	if err != nil {
 		t.Fatal(err)
 	}
-	assert.Equal(t, "2021-09-01 21:24:29", FormatMillis(date.UnixMilli()))
+
+	// assign expected with different timezone result from environment variable
+	var expected string
+	switch os.Getenv("TZ") {
+	case "Asia/Hong_Kong":
+		expected = "2021-09-01T22:24:29+08"
+	case "Europe/London":
+		expected = "2021-09-01T13:24:29+00"
+	case "America/New_York":
+		expected = "2021-09-01T05:24:29-04"
+	default:
+		otherTz, err := time.LoadLocation("Asia/Hong_Kong")
+		if err != nil {
+			t.Fatal(err)
+		}
+		ti, err := ToTimeZone(iso, otherTz)
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected = FormatISO(ti)
+	}
+	assert.Equal(t, expected, FormatISO(date))
 }
